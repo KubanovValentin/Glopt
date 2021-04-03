2c636ce3a64f3697314d1695918a8f544942ac71
KubanovValentin <delo.biss13@gmail.com>
2021-04-03 18:22:06 +0300

    stylization section-feed and adaptation/add carusel

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,37 @@
+// $(document).ready(function() {
+
+//     $('.carousel__inner').slick({
+//             speed: 1200,
+//             dcenterMode: true,
+//             centerPadding: '60px',
+//             slidesToShow: 3,
+//             responsive: [
+//                 {
+//                   breakpoint: 768,
+//                   settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 3
+//                   }
+//                 },
+//                 {
+//                   breakpoint: 480,
+//                   settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 1
+//                   }
+//                 }
+//               ]
+//         // adaptiveHeight: true,
+//         // prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         // nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+        
+//     });
+// });
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu_item'),
@@ -14,4 +48,8 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('menu_active');
         })
     })
-})
+});
+
+$(function() {
+  $('#dg-container').gallery();
+});
